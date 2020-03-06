422cdac315ec890a54b34b6a6bc1a8e69136bc59
Muhammad Wafa <wafax.4@gmail.com>
2020-03-07 02:56:46 +0700

    add grouping submission menu

--- bot/server/components/SubmissionMenu.js
+++ bot/server/components/SubmissionMenu.js
@@ -4,35 +4,53 @@ import store from '../store'
 const SubmissionMenu = () => {
     const { state, dispatch } = useContext(store)
     const { deadline, submission } = state
+    const groups = deadline.list
+                  .map(item => item.kode_praktikum.slice(0, -2))
+                  .filter((v, i, a) => a.indexOf(v) === i)
+                  .map(item => ({
+                    name: item,
+                    list: deadline.list.filter(i => i.kode_praktikum.slice(0, -2) === item)
+                  }))
     return(
       <aside className="menu">
-        <p className="menu-label">
-          Kode Praktikum
-        </p>
         <ul className="menu-list">
           <li>
             <a 
-              className={!submission.activeDeadline? "is-active": ""}
+              className={!submission.activeDeadline ? "is-active" : ""}
               onClick={() => dispatch({type: "SET_SHOW_DEADLINE", payload: ''})}
               >
                 All
             </a>
           </li>
-          {
-            deadline.list.map((item, key) => {
-              return (
-                <li key={key}>
-                  <a 
-                    className={submission.activeDeadline === item.kode_praktikum ? "is-active": ""}
-                    onClick={() => dispatch({type: "SET_SHOW_DEADLINE", payload: item.kode_praktikum})}
-                    >
-                    {item.kode_praktikum}
-                  </a>
-                </li>
-              )
-            })
-          }
         </ul>
+        {
+          groups.map((group, key) => {
+            return (
+              <div key={key}>
+              <hr/>
+              <p className="menu-label">
+                {group.name}
+              </p>
+              <ul className="menu-list">
+                {
+                  group.list.map((item, key) => {
+                    return (
+                      <li key={key}>
+                        <a 
+                          className={submission.activeDeadline === item.kode_praktikum ? "is-active" : ""}
+                          onClick={() => dispatch({type: "SET_SHOW_DEADLINE", payload: item.kode_praktikum})}
+                          >
+                          {item.kode_praktikum}
+                        </a>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+              </div>
+            )
+          })
+        }
       </aside>
     )
 }
